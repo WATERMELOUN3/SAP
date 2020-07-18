b660c5fcc27bac129c6c53b010c4d4e772df874b
WATERMELOUN3 <alexis.gelin@outlook.fr>
2020-07-18 03:20:14 +0200

    Created project

---
+++ SAPArchive.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SAP
+{
+    
+    public class SAPArchive
+    {
+        private Dictionary<string, long> _files;
+
+        public SAPArchive(string path)
+        {
+            
+        }
+    }
+}
